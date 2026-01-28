1d411d13bd5e4b9d5b0e7829d2bdbaefa2429252
Flyan-Fu <2910757458@qq.com>
2026-01-28 21:50:10 +0800

    Finish Problem A

--- IT5003-Data_Structure_and_Algorithm/PS2/Problem A.py
+++ IT5003-Data_Structure_and_Algorithm/PS2/Problem A.py
@@ -3,13 +3,46 @@ Maximally Unfair Fair IKEA Trip
 '''
 k = int(input())
 n = int(input())
+index = 0
 stuffs = []
+sum_weight = 0
+part1_weight = 0
+part2_weight = 0
+carry = []
 for _ in range(n):
-    stuffs.append(int,tuple(input().split()))
-for _ in range(n-1):
-    if int(stuffs[_][1]) >  int(stuffs[_+1][1]):
-        stuffs[_][1],stuffs[_+1][1] = stuffs[_+1][1],stuffs[_][1]
-        
-print(stuffs)
-
+    stuff = input().split()
+    stuff[1] = int(stuff[1])
+    stuff.append(index)
+    index += 1
+    stuffs.append(stuff)
+# Sort priority: Weight > Index
+stuffs.sort(key=lambda x: (x[1], x[2]))
+if n % k:
+    for i in range(n//k+1):
+        part1_weight +=  stuffs[i][1]
+    for j in range(n//k+1,2*(n//k)+1):
+        part2_weight +=  stuffs[j][1]
+    if part1_weight < part2_weight:
+        sum_weight = part1_weight
+        print(sum_weight)
+        carry = stuffs[0: n//k+1]
+        carry.sort(key=lambda x: x[0])
+# Print priority: Name
+        for i in range(len(carry)):
+            print(carry[i][0])
+    else:
+        sum_weight = part1_weight - stuffs[n//k][1]
+        print(sum_weight)
+        carry = stuffs[0: n//k]
+        carry.sort(key=lambda x: x[0])
+        for i in range(len(carry)):
+            print(carry[i][0])
+else:
+    for _ in range(n//k):
+        sum_weight +=  stuffs[_][1]  
+    print(sum_weight)
+    carry = stuffs[0: n//k]
+    carry.sort(key=lambda x: x[0])
+    for i in range(len(carry)):
+        print(carry[i][0])
     
